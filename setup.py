5a53b05296c86832df39c3d0472d4c4bf270a171
Matt Barker <105945282+m-barker@users.noreply.github.com>
2022-08-17 14:29:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+def get_readme():
+    with open("README.md") as readme_file:
+        return readme_file.read()
+
 setup(
     name="fibs-reporter",
     version="0.1.0",
+    description="Automatically generate a pdf report containing feature importance, 
+                "baseline modelling, spurious correlation detection, and more, from a single command line input",
+    url="https://github.com/m-barker/fibs-reporter/",
+    long_description = get_readme(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
